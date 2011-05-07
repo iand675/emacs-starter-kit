1946c475b789622ae4f203de2e37796be547a971
Ian Duncan <iand675@gmail.com>
2011-05-07 02:51:52 -0500

    Added org mode config, custom keyboard shortcuts, little bit of mac friendliness.

--- custom.el
+++ custom.el
@@ -1,13 +1,14 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(cc-other-file-alist (quote (("\\.cc\\'" (".hh" ".h")) ("\\.hh\\'" (".cc" ".C")) ("\\.m\\'" (".h")) ("\\.c\\'" (".h")) ("\\.h\\'" (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m")) ("\\.C\\'" (".H" ".hh" ".h")) ("\\.H\\'" (".C" ".CC")) ("\\.CC\\'" (".HH" ".H" ".hh" ".h")) ("\\.HH\\'" (".CC")) ("\\.c\\+\\+\\'" (".h++" ".hh" ".h")) ("\\.h\\+\\+\\'" (".c++")) ("\\.cpp\\'" (".hpp" ".hh" ".h")) ("\\.hpp\\'" (".cpp")) ("\\.cxx\\'" (".hxx" ".hh" ".h")) ("\\.hxx\\'" (".cxx")))))
- '(javascript-indent-level 2))
+ '(javascript-indent-level 2)
+ '(org-agenda-files (quote ("~/todo.org"))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "microsoft" :family "Consolas")))))
--- ian.el
+++ ian.el
@@ -15,11 +15,25 @@
 (load-theme 'tango-dark)
 
 (require 'sr-speedbar)
-(sr-speedbar-open)
+;; (sr-speedbar-open)
 
 (load (concat dotfiles-dir "vendor/haskell-mode/haskell-site-file"))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'yas/minor-mode)
+;; (speedbar-add-supported-extension ".hs")
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/")
+(require 'magit)
+(require 'magit-svn)
 
 (menu-bar-mode)
+
+(global-set-key (kbd "C-x C-a") 'magit-status)
+(global-set-key (kbd "C-x C-m") 'ns-toggle-fullscreen)
+(global-set-key (kbd "C-x <C-return>") 'ns-toggle-fullscreen)
+(global-set-key (kbd "C-x t") 'hippie-expand)
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+(setq org-agenda-files "~/index.org")
